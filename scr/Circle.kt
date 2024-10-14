b64162bc93bf3956500f2842e57a0e9561a4138d
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-10-14 19:53:32 +0800

    Update Circle.kt

--- scr/Circle.kt
+++ scr/Circle.kt
@@ -1,5 +1,14 @@
-class Circle : Figure(0) {
-    override fun area(): Float {
-        return  0.toFloat();
+class Circle(var x: Int = 0, var y: Int = 0, var radius: Int = 1) : Movable, Transforming, Figure(0) {
+    override fun move(dx: Int, dy: Int) {
+        x += dx
+        y += dy
     }
+    override fun area(): Float = (Math.PI * radius * radius).toFloat()
+
+    override fun resize(zoom: Int) {
+        radius *= zoom
+    }
+    override fun rotate(direction: RotateDirection, centerX: Int, centerY: Int) {
+    }
+    override fun toString(): String = "Circle(x=$x, y=$y, radius=$radius)"
 }
